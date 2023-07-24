d375bbe03d8dc517db62025c197e5aa1c56f9d76
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-24 14:02:42 +0545

    fixed: order, products and users model created

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: true,
+    },
+    img: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
