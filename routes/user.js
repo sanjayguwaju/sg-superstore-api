f29a22bca789a09a0125939b4459bbb3f13c081b
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-25 12:48:15 +0545

    fixed: password ommited while retriviewing the userdata

--- routes/user.js
+++ routes/user.js
@@ -52,12 +52,12 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
 });
 
 //GET ALL USER
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
+router.get("/", async (req, res) => {
   const query = req.query.new;
   try {
     const users = query
-      ? await User.find().sort({ _id: -1 }).limit(5)
-      : await User.find();
+      ? await User.find().sort({ _id: -1 }).limit(5).select("-password")
+      : await User.find().select("-password");;
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
