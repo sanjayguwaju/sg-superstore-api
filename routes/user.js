926cb78e5ab6a11f755040ebb27f273683752b55
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-28 10:56:20 +0545

    adduser routes added for the user

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,16 @@ const {
 
 const router = require("express").Router();
 
+router.post("/adduser", async (req,res)=> {
+  const newUser = new User(req.body);
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 //UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
