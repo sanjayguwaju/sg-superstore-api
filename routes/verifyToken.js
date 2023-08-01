835ae0cf82481d00093ed99c79dacbaf7f803a4f
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-01 16:02:11 +0545

    code optimized and refactored

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,50 +1,58 @@
 const jwt = require("jsonwebtoken");
+const util = require('util');
 
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.token;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) res.status(403).json("Token is not valid!");
-      req.user = user;
+// Promisify jwt.verify()
+const verify = util.promisify(jwt.verify);
+
+// Common error message
+const errorMessage = "You do not have sufficient permissions to perform this action!";
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    const token = authHeader.slice(7, authHeader.length).trimLeft(); // Extract token
+    try {
+      req.user = await verify(token, process.env.JWT_SEC);
       next();
-    });
+    } catch (err) {
+      return res.status(403).json({ message: "Invalid token!" });
+    }
   } else {
-    return res.status(401).json("You are not authenticated!");
+    return res.status(401).json({ message: "Authentication required!" });
   }
 };
 
-const verifyTokenAndAuthorization = (req, res, next) => {
-  verifyToken(req, res, () => {
+const verifyTokenAndAuthorization = async (req, res, next) => {
+  await verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
-      res.status(403).json("You are not alowed to do that!");
+      return res.status(403).json({ message: errorMessage });
     }
   });
 };
 
-const verifyTokenAndAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
+const verifyTokenAndAdmin = async (req, res, next) => {
+  await verifyToken(req, res, () => {
     if (req.user.isAdmin) {
       next();
     } else {
-      res.status(403).json("You are not alowed to do that!");
+      return res.status(403).json({ message: errorMessage });
     }
   });
 };
 
-const verifyUserRole = (req, res, next) => {
-  const allowedRoles = req.route.allowedRoles || []; // get the allowed roles for the current route, or use an empty array if not set
-  const userRole = req.user.role; // get the role of the authenticated user
-  if (allowedRoles.length === 0 || allowedRoles.includes(userRole)) { // check if the user's role is allowed for the current route
-    next();
-  } else {
-    res.status(403).json("You are not allowed to do that!");
-  }
+const verifyUserRole = async (req, res, next) => {
+  await verifyToken(req, res, () => {
+    const allowedRoles = req.route.allowedRoles || []; // get the allowed roles for the current route, or use an empty array if not set
+    const userRole = req.user.role; // get the role of the authenticated user
+    if (allowedRoles.length === 0 || allowedRoles.includes(userRole)) { // check if the user's role is allowed for the current route
+      next();
+    } else {
+      return res.status(403).json({ message: errorMessage });
+    }
+  });
 };
-
-
 
 module.exports = {
   verifyToken,
